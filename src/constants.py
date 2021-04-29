6a1b0d6119910209f6750acd40f640e966fa109b
Simon Mathis <svm34@cam.ac.uk>
2021-04-29 11:54:01 +0100

    feature: add GEDI path constants and crs

--- src/constants.py
+++ src/constants.py
@@ -14,7 +14,9 @@ LOG_PATH.mkdir(parents=True, exist_ok=True)
 
 #  Data related paths
 DATA_PATH = Path("/gws/nopw/j04/forecol/data")
-
+GEDI_PATH = DATA_PATH / "GEDI"
+GEDI_L2A_PATH = GEDI_PATH / "level2A"
+JRC_PATH = DATA_PATH / "JRC"
 
 # ---------------- LOGGING CONSTANTS ----------------
 DEFAULT_FORMATTER = logging.Formatter(
@@ -25,3 +27,7 @@ DEFAULT_FORMATTER = logging.Formatter(
 )
 DEFAULT_LOG_FILE = LOG_PATH / "default_log.log"
 DEFAULT_LOG_LEVEL = logging.DEBUG  # verbose logging per default
+
+# ---------------- PROJECT CONSTANTS ----------------
+# Coordinate reference systems (crs)
+WGS84 = "EPSG:4326"  # WGS84 standard crs (latitude, longitude)
