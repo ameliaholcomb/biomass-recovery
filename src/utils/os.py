9d8ef3d4761e0f21e06a245ef2d09dbaccfeb302
Simon Mathis <svm34@cam.ac.uk>
2021-05-05 16:01:50 +0100

    refactor: rename show_content -> list_content

--- src/utils/os.py
+++ src/utils/os.py
@@ -2,5 +2,5 @@
 import pathlib
 
 
-def show_content(path: pathlib.Path, glob_query: str = "*") -> list[pathlib.Path]:
+def list_content(path: pathlib.Path, glob_query: str = "*") -> list[pathlib.Path]:
     return list(path.glob(glob_query))
