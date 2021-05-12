1f97229051fa5209c5a11c1f76eb7ee4b585b1dd
Simon Mathis <svm34@cam.ac.uk>
2021-05-12 10:05:48 +0100

    feature: add environment variables for planet download

--- src/constants.py
+++ src/constants.py
@@ -1,12 +1,16 @@
 """Module contains all project wide constants."""
 import logging
+import os
 from pathlib import Path
+
+import dotenv
 
 # ---------------- PATH CONSTANTS -------------------
 #  Source folder path
 constants_path = Path(__file__)
 SRC_PATH = constants_path.parent
 PROJECT_PATH = SRC_PATH.parent
+dotenv.load_dotenv()
 
 # Log relatedd paths
 LOG_PATH = PROJECT_PATH / "logs"
@@ -14,9 +18,17 @@ LOG_PATH.mkdir(parents=True, exist_ok=True)
 
 #  Data related paths
 DATA_PATH = Path("/gws/nopw/j04/forecol/data")
+
 GEDI_PATH = DATA_PATH / "GEDI"
+GEDI_L1B_PATH = GEDI_PATH / "level1B"
 GEDI_L2A_PATH = GEDI_PATH / "level2A"
+
 JRC_PATH = DATA_PATH / "JRC"
+
+PLANET_PATH = DATA_PATH / "Planet"
+
+# ---------------- API KEYS -------------------------
+PLANET_API_KEY = os.getenv("PLANET_API_KEY")
 
 # ---------------- LOGGING CONSTANTS ----------------
 DEFAULT_FORMATTER = logging.Formatter(
