635f7e8645cca7b6716de906cb6d510ecaa4a3cf
Simon Mathis <svm34@cam.ac.uk>
2021-06-01 18:08:22 +0100

    feature: add routine to find latest version of file in folder

--- src/utils/os.py
+++ src/utils/os.py
@@ -4,3 +4,9 @@ import pathlib
 
 def list_content(path: pathlib.Path, glob_query: str = "*") -> list[pathlib.Path]:
     return list(path.glob(glob_query))
+
+
+def latest_matching_file(path: pathlib.Path) -> pathlib.Path:
+    path = pathlib.Path(path)
+    options = list(str(filepath) for filepath in path.parent.glob(f"{path.name}*"))
+    return pathlib.Path(sorted(options)[-1])
