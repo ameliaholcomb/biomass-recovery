8f401d18bf3217668aa6f089e30ecc02fb149601
Simon Mathis <svm34@cam.ac.uk>
2021-06-01 18:14:59 +0100

    feature: add filter and logging to unzip routine

--- src/utils/unzip.py
+++ src/utils/unzip.py
@@ -1,8 +1,16 @@
 """Util to unzip .zip files to a given path in Python"""
+import logging
 import pathlib
+import tempfile
 import zipfile
+from typing import Callable, Optional
 
 from tqdm.autonotebook import tqdm
+
+from src.utils.logging import get_logger
+
+logger = get_logger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def unzip(
@@ -10,6 +18,7 @@ def unzip(
     out_path: pathlib.Path,
     overwrite: bool = False,
     remove_archive_name: bool = True,
+    filter_func: Optional[Callable[[str], bool]] = None,
 ) -> bool:
     """
     Extract all files in a given .zip file at `zip_path` to `out_path`.
@@ -21,6 +30,10 @@ def unzip(
             Defaults to False.
         remove_archive_name (bool, optional): Whether to remove the name of the zip
             archive from the path when extracting the zip files. Defaults to True.
+        filter_func (Callable, optional): Pass a function which will be applied to the
+            filenames (as str) in the zip directory to filter out only the and extract
+            only those zip files for which `filter_func` evaluates to true. Defaults
+            to None.
 
     Returns:
         bool: True, iff the extraction succeeded.
@@ -33,9 +46,12 @@ def unzip(
     with zipfile.ZipFile(zip_path, "r") as zip_archive:
         # Get all files in zip_archive
         files = [elem for elem in zip_archive.infolist() if not elem.is_dir()]
+        if filter_func is not None:
+            files = [elem for elem in files if filter_func(elem.filename)]
+            logger.debug("Extracting files: %s", [elem.filename for elem in files])
 
         # Check for each file if it exists already. If yes, skip. If not, extract.
-        files_progress = tqdm(files)
+        files_progress = tqdm(files, leave=False)
         for compressed_file in files_progress:
             # Get relative path of file in zip archive
             if remove_archive_name:
@@ -49,11 +65,28 @@ def unzip(
             )
 
             # Check for existing files
-            if not overwrite and (out_path / relative_path).exists():
+            save_path = out_path / relative_path
+            if not overwrite and save_path.exists():
                 # print(f"File {out_path / relative_path} exists.")
                 continue
             else:
-                zip_archive.extract(compressed_file, path=out_path / relative_path)
-                assert (out_path / relative_path).exists()
+                # To create a file at a certain out path, first extract to
+                # temporary directory and then move to desired out path
+                with tempfile.TemporaryDirectory(prefix=str(out_path) + "/") as tmp_dir:
+                    logger.debug("Temporary dir: %s", tmp_dir)
+                    tmp_path = pathlib.Path(tmp_dir)
+                    zip_archive.extract(compressed_file, path=tmp_path)
+                    extracted_file = tmp_path / compressed_file.filename
+                    logger.debug(
+                        "Extracted file: %s exists: %s",
+                        extracted_file,
+                        extracted_file.exists(),
+                    )
+                    logger.debug("Moving file to %s", save_path)
+                    save_path.parent.mkdir(parents=True, exist_ok=True)
+                    extracted_file.replace(save_path)
+
+                assert save_path.exists()
+                assert not save_path.is_dir()
 
     return True
