7a1b2aa93f31ccc4404087e58d3085b10feea56e
Simon Mathis <svm34@cam.ac.uk>
2021-06-24 12:34:13 +0100

    feature: add holoviews visualisation parameters

---
+++ src/visualisation/visualisation_params.py
@@ -0,0 +1,44 @@
+"""Styling parameters and legend for visualisations"""
+
+opt_size = {"width": 750, "height": 320, "show_grid": True}
+
+opt_densities = {
+    "colorbar": True,
+    "tools": ["hover"],
+    "cmap": "magma",
+    "cnorm": "linear",
+}
+
+opt_biomass = {
+    "colorbar": True,
+    "tools": ["hover"],
+    "cmap": "kgy_r",
+    "cnorm": "eq_hist",
+}
+
+opt_metrics = {
+    "colorbar": True,
+    "tools": ["hover"],
+    "cnorm": "eq_hist",
+    "cmap": "bgy_r",
+}
+
+legend = {
+    "longo_biomass": dict(name="Longo ACD", unit="kgC/m2"),
+    "max": dict(name="Max-height", unit="m"),
+    "mean": dict(name="Mean-height", unit="m"),
+    "interquartile_range": dict(name="IQR-height", unit="m"),
+    "canopy_height_model": dict(name="Canopy height", unit="m"),
+    "kurtosis": dict(name="Kurtosis", unit="1"),
+    "quantile0.05": dict(name="5th-Percentile-height", unit="m"),
+    "quantile0.1": dict(name="10th-Percentile-height", unit="m"),
+    "quantile0.2": dict(name="20th-Percentile-height", unit="m"),
+    "quantile0.95": dict(name="95th-Percentile-height", unit="m"),
+    "quantile0.98": dict(name="98th-Percentile-height", unit="m"),
+    "point_density": dict(name="Point density", unit="1/m2"),
+    "pulse_density": dict(name="Pulse density", unit="1/m2"),
+    "ground_point_density": dict(name="Ground point density", unit="1/m2"),
+    "n_points": dict(name="No. points", unit="1"),
+    "n_pulses": dict(name="No. pulses", unit="1"),
+    "n_ground_points": dict(name="No. ground points", unit="1"),
+}
