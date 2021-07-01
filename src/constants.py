95c439c1ab469c4297f03e8a282d91570c38ee48
Simon Mathis <svm34@cam.ac.uk>
2021-07-01 09:21:42 +0100

    fix: sirgas typo

--- src/constants.py
+++ src/constants.py
@@ -44,7 +44,8 @@ DEFAULT_LOG_LEVEL = logging.DEBUG  # verbose logging per default
 # ---------------- PROJECT CONSTANTS ----------------
 # Coordinate reference systems (crs)
 WGS84 = "EPSG:4326"  # WGS84 standard crs (latitude, longitude)
-WEBMERCATOR = "EPSG:3857"
+WEBMERCATOR = "EPSG:3857"  # CRS for web maps
+SIRGAS_BRAZIL = "EPSG:5880"  # Polyconic projected CRS for Brazil
 
 WGS84_UTM18S = "EPSG:32718"  # https://epsg.io/32718
 WGS84_UTM19S = "EPSG:32719"  # https://epsg.io/32719
@@ -52,17 +53,19 @@ WGS84_UTM20S = "EPSG:32720"  # https://epsg.io/32720
 WGS84_UTM21S = "EPSG:32721"  # https://epsg.io/32721
 WGS84_UTM22S = "EPSG:32722"  # https://epsg.io/32722
 WGS84_UTM23S = "EPSG:32723"  # https://epsg.io/32723
+WGS84_UTM24S = "EPSG:32724"  # https://epsg.io/32724
 
-SIGRAS2000_UTM18S = "EPSG:31978"  # https://epsg.io/31978
-SIGRAS2000_UTM19S = "EPSG:31979"  # https://epsg.io/31979
-SIGRAS2000_UTM20S = "EPSG:31980"  # https://epsg.io/31980
-SIGRAS2000_UTM21S = "EPSG:31981"  # https://epsg.io/31981
-SIGRAS2000_UTM22S = "EPSG:31982"  # https://epsg.io/31982
-SIGRAS2000_UTM23S = "EPSG:31983"  # https://epsg.io/31983
+SIRGAS2000_UTM18S = "EPSG:31978"  # https://epsg.io/31978
+SIRGAS2000_UTM19S = "EPSG:31979"  # https://epsg.io/31979
+SIRGAS2000_UTM20S = "EPSG:31980"  # https://epsg.io/31980
+SIRGAS2000_UTM21S = "EPSG:31981"  # https://epsg.io/31981
+SIRGAS2000_UTM22S = "EPSG:31982"  # https://epsg.io/31982
+SIRGAS2000_UTM23S = "EPSG:31983"  # https://epsg.io/31983
+SIRGAS2000_UTM24S = "EPSG:31984"  # https://epsg.io/31984
 
 # ---------------- DATABASE CONSTANTS ----------------
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")  # JASMIN database server
+DB_NAME = os.getenv("DB_NAME")  # Database for GEDI shots
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_CONFIG = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
