068380f17d060fbe8649d752a57f24ee94ba3072
Simon Mathis <svm34@cam.ac.uk>
2021-07-04 10:54:46 +0100

    feature: add point decimation option and heightmap filtering to lidar processing

--- src/processing/lidar_processing_script.R
+++ src/processing/lidar_processing_script.R
@@ -15,23 +15,11 @@ option_list <- list(
         help = "Path in which to save the output results",
         metavar = "character"
     ),
-    make_option(c("--save_prefix"),
-        type = "character",
-        default = "",
-        help = "Prefix to use when saving files",
-        metavar = "character"
-    ),
     make_option(c("--save_pattern"),
         type = "character",
         default = "{XLEFT}_{YBOTTOM}",
         help = "Pattern to use for saving files",
         metavar = "character"
-    ),
-    make_option(c("--perform_check"),
-        type = "logical",
-        default = TRUE,
-        help = "Whether to perform a deep check on the files",
-        metavar = "bool"
     ),
     make_option(c("--save_intermediates"),
         type = "logical",
@@ -59,30 +47,74 @@ option_list <- list(
             0 means file by file.",
         metavar = "number"
     ),
+    make_option(c("--lidar_crs"),
+        type = "character",
+        default = NULL,
+        help = "Proj4 string for the crs of the lidar data.",
+        metavar = "character"
+    ),
+    make_option(c("--desired_density"),
+        type = "double",
+        default = 6,
+        help = "Desired point/pulse density after point cloud decimation.
+         Defaults to 6.",
+        metavar = "number"
+    ),
+    make_option(c("--decimation_gridsize"),
+        type = "double",
+        default = 5,
+        help = "Grid size to use for decimating the points/pulses. Defautls
+         to 5.",
+        metavar = "number"
+    ),
+    make_option(c("--decimate_by_pulse"),
+        type = "logical",
+        default = TRUE,
+        help = "Whether to decimate by pulse (if TRUE) or by points
+         (if FALSE). Defaults to True.",
+        metavar = "bool"
+    ),
     make_option(c("--filter_quantile"),
         type = "double",
         default = 0.95,
-        help = "Quantile to use for as reference for height noise filtering.",
+        help = "Quantile to use for as reference for height noise filtering.
+         Defaults to 0.95.",
         metavar = "number"
     ),
     make_option(c("--filter_sensitivity"),
         type = "double",
         default = 1.1,
         help = "Points higher than `sensitivity`*`height at filter_quantile`
-            will be filtered",
+            will be filtered. Defaults to 1.1 (10%).",
         metavar = "number"
     ),
     make_option(c("--filter_gridsize"),
         type = "integer",
         default = 10,
-        help = "Grid size at which the filter quantile will be computed",
+        help = "Grid size at which the filter quantile will be computed.
+         Defaults to 10.",
         metavar = "number"
+    ),
+    make_option(c("--perform_check"),
+        type = "logical",
+        default = FALSE,
+        help = "Whether to perform a deep check on the files. Defaults to
+         FALSE.",
+        metavar = "bool"
+    ),
+    make_option(c("--perform_decimation"),
+        type = "logical",
+        default = FALSE,
+        help = "Whether to decimate the point cloud to a targeted pulse / point
+         density before running all subsequent processing steps. Defaults to
+         FALSE",
+        metavar = "bool"
     ),
     make_option(c("--perform_classification"),
         type = "logical",
         default = TRUE,
         help = "Whether to classify the ground.
-         If FALSE, uses existing Classifiation.",
+         If FALSE, uses existing Classifiation. Defaults to TRUE.",
         metavar = "bool"
     ),
     make_option(c("--perform_normalisation"),
@@ -97,21 +129,29 @@ option_list <- list(
         help = "Whether to filter the data.",
         metavar = "bool"
     ),
+    make_option(c("--overwrite"),
+        type = "logical",
+        default = FALSE,
+        help = "Whether to overwrite existing files.",
+        metavar = "number"
+    ),
     make_option(c("--seed"),
         type = "integer",
-        default = 32456,
+        default = 94153,
         help = "Random seed for stochastic computations",
         metavar = "number"
     )
 )
 
 # Import custom scripts
-r_script_path <- "/home/users/svm/Code/gedi_biomass_mapping/src/processing/"
-timestamp_path <- paste0(r_script_path, "timestamp.R")
+R_SCRIPT_PATH <- "/home/users/svm/Code/gedi_biomass_mapping/src/processing/"
+HEIGHT_MAP_PATH <- "/gws/nopw/j04/forecol/data/Amazon_height_map/rfHeightRasterCor80v14042020.tif"
+
+timestamp_path <- paste0(R_SCRIPT_PATH, "timestamp.R")
 source(timestamp_path)
-las_clean_path <- paste0(r_script_path, "las_clean.R")
+las_clean_path <- paste0(R_SCRIPT_PATH, "las_clean.R")
 source(las_clean_path)
-las_indexing_path <- paste0(r_script_path, "las_indexing.R")
+las_indexing_path <- paste0(R_SCRIPT_PATH, "las_indexing.R")
 source(las_indexing_path)
 
 # Create first timestamp
@@ -176,16 +216,55 @@ write(
 )
 summary(ctg)
 
+# Step 0: Optionally decimate point cloud ===================================
+if (opt$perform_decimation) {
+    write(
+        paste("\n", timestamp()$now, ": ... Decimating pointcloud ...\n"),
+        stdout()
+    )
+    if (opt$save_intermediates) {
+        lidR::opt_output_files(ctg) <- paste0(
+            opt$save_path,
+            "/decimated/",
+            opt$save_pattern
+        )
+    } else {
+        lidR::opt_output_files(ctg) <- paste0(
+            tempdir(), "/{ID}_{XLEFT}_{YBOTTOM}"
+        )
+    }
+
+    if (opt$overwrite || !(file.exists(paste0(opt$save_path, "/decimated/")) &&
+        length(list.files(
+            paste0(opt$save_path, "/decimated/")
+        )) > 0)
+    ) {
+        # Ensure files are spatially indexed to greatly speed up computations
+        ensure_lax_index(ctg, verbose = TRUE)
+        # Perform point decimation
+        ctg <- lidR::decimate_points(ctg,
+            algorithm = lidR::homogenize(
+                density = opt$desired_density,
+                res = opt$decimation_gridsize,
+                use_pulse = opt$decimate_by_pulse
+            )
+        )
+    } else {
+        ctg <- lidR::readLAScatalog(paste0(opt$save_path, "/decimated/"))
+    }
+}
+
 
 # Step 1: Classify ground ===================================================
-write(paste("\n", timestamp()$now, ": ... Classifying ground ...\n"), stdout())
-
 if (opt$perform_classification) {
+    write(
+        paste("\n", timestamp()$now, ": ... Classifying ground ...\n"),
+        stdout()
+    )
     if (opt$save_intermediates) {
         lidR::opt_output_files(ctg) <- paste0(
             opt$save_path,
             "/csf_ground/",
-            opt$save_prefix,
             opt$save_pattern
         )
     } else {
@@ -210,14 +289,15 @@ if (opt$perform_classification) {
 
 
 # Step 2: Perform height normalisation ======================================
-write(paste("\n", timestamp()$now, ": ... Normalising heights ...\n"), stdout())
-
 if (opt$perform_normalisation) {
+    write(
+        paste("\n", timestamp()$now, ": ... Normalising heights ...\n"),
+        stdout()
+    )
     if (opt$save_intermediates) {
         lidR::opt_output_files(ctg) <- paste0(
             opt$save_path,
             "/normalised/",
-            opt$save_prefix,
             opt$save_pattern
         )
     } else {
@@ -236,13 +316,38 @@ if (opt$perform_normalisation) {
 
 
 # Step 3: Filter noise ======================================================
-write(paste("\n", timestamp()$now, ": ... Filtering noise ...\n"), stdout())
-
 if (opt$perform_filter) {
+    write(
+        paste("\n", timestamp()$now, ": ... Filtering noise ...\n"),
+        stdout()
+    )
+    if (!is.null(opt$lidar_crs)) {
+        # Load height map
+        height_map <- raster::raster(HEIGHT_MAP_PATH)
+        # Get ALS raster
+        extents <- raster::raster(raster::extent(ctg), crs = opt$lidar_crs)
+        # Transform extents to WGS84
+        extents <- raster::projectExtent(
+            extents,
+            crs = "+proj=longlat +datum=WGS84 +no_defs"
+        )@extent
+        # Crop tree height map values from tree height map
+        height_map <- raster::crop(height_map, extents)
+        # Determine max filter value (either 10% above max value or more
+        #  than 5m, whichever is higher)
+        filter_height <- max(c(
+            height_map@data@max * 1.1,
+            height_map@data@max + 5
+        ))
+        lidR::opt_filter(ctg) <- paste("-drop_z_above", filter_height)
+        write(
+            paste("\nSet max filter height to", filter_height, ".\n"),
+            stdout()
+        )
+    }
     lidR::opt_output_files(ctg) <- paste0(
         opt$save_path,
         "/processed/",
-        opt$save_prefix,
         opt$save_pattern
     )
     # set buffer to min possible value
