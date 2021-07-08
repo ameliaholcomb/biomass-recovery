0520dc062afd441ab5829636b58fb9e45927b14a
Simon Mathis <svm34@cam.ac.uk>
2021-07-08 10:36:01 +0100

    feature: add util to detect lidR unprocessed tiles

--- src/processing/utils.R
+++ src/processing/utils.R
@@ -1,3 +1,35 @@
+unprocessed_files <- function(ctg, tolerance = 1) {
+
+    # Initialise by activating processing for all files
+    is_unprocessed <- rep(TRUE, length(ctg))
+
+    # Extract spatial information of catalog
+    out_dir <- dirname(ctg@output_options$output_files)
+    out_ctg <- lidR::readLAScatalog(out_dir)
+    processed_polygon <- lidR::as.spatial(out_ctg)
+
+    # For each file in the catalog to process, check whether
+    #  it is already contained in the processed output
+    for (i in 1:length(ctg)) {
+
+        # Extract the polygon shape of each file in ctg
+        single_poly <- as(
+            raster::extent(
+                ctg@polygons[[i]]@Polygons[[1]]@coords
+            ) - tolerance,
+            "SpatialPolygons"
+        )
+
+        # Update the input ctg files that should be processed as only
+        #  those which are not already contained in the processed polygon
+        is_unprocessed[[i]] <- !rgeos::gContains(processed_polygon, single_poly)
+    }
+    return(is_unprocessed)
+}
+
+
+
+
 N_CORES <- parallel::detectCores()
 n_workers <- NULL
 
