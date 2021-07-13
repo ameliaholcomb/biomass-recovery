12b430eac42075ef25b7be0deb6ad2e1f33ceaf2
Simon Mathis <svm34@cam.ac.uk>
2021-07-13 09:22:51 +0100

    feature: add visualisation tool and update visualisation parameters

---
+++ src/processing/gedi_recovery_analysis.py
@@ -0,0 +1,104 @@
+import logging
+
+import numba
+import numpy as np
+import pandas as pd
+
+from src.constants import WGS84
+from src.data.gedi_loading import load_gedi_data
+from src.data.jrc_loading import load_jrc_data
+from src.processing.jrc_processing import compute_recovery_period
+from src.utils.logging import get_logger
+
+logger = get_logger(__file__)
+logger.setLevel(logging.DEBUG)
+
+
+@numba.njit
+def argnearest(array, values):
+    argmins = np.zeros_like(values, dtype=np.int64)
+    for i, value in enumerate(values):
+        argmins[i] = (np.abs(array - value)).argmin()
+    return argmins
+
+
+def overlay_gedi_shots_and_recovery_raster(gedi_shots, recovery_period):
+
+    shot_ids = gedi_shots.shot_number.values
+    x_inds = argnearest(recovery_period.x.data, gedi_shots.lon_highestreturn.values)
+    y_inds = argnearest(recovery_period.y.data, gedi_shots.lat_highestreturn.values)
+
+    logger.info("Filtering shots")
+    # This loop is quite fast in my test, does not need numba.
+    filtered_shots = []
+    for shot_id, y_ind, x_ind in zip(shot_ids, y_inds, x_inds):
+        recovery_at_shot = recovery_period.data[y_ind, x_ind]
+
+        # If recovery_period at shot > 0
+        if recovery_at_shot > 0:
+            # Check the surrounding pixels as well
+            recovery_around_shot = recovery_period.data[
+                y_ind - 1 : y_ind + 2, x_ind - 1 : x_ind + 2
+            ]
+            # If the surrouding pixels also have recovery_period > 0 ...
+            if recovery_around_shot.shape == (3, 3) and np.alltrue(
+                recovery_around_shot > 0
+            ):
+                if np.alltrue(recovery_around_shot == recovery_at_shot):
+                    # ... and all have the same recovery length, set quality flag 2
+                    filtered_shots.append((shot_id, recovery_at_shot, 2))
+                else:
+                    # ... and have different recovery lengths, set quality flag 1
+                    filtered_shots.append((shot_id, recovery_at_shot, 1))
+            else:
+                # ... else, set quality flag 0
+                filtered_shots.append((shot_id, recovery_at_shot, 0))
+    # Return the shots
+    logger.debug("Converting to array")
+    return np.array(filtered_shots, dtype=np.uint64)
+
+
+def compute_gedi_recovery(geometry, year, crs: str = WGS84):
+
+    # Load GEDI data within tile
+    logger.info("Loading GEDI shots for %s", year)
+    gedi_shots = load_gedi_data(
+        columns=[
+            "shot_number",
+            "absolute_time",
+            "lon_highestreturn",
+            "lat_highestreturn",
+            "geometry",
+        ],
+        geometry=geometry,
+        crs=crs,
+        start_time=f"{year}-01-01",
+        end_time=f"{year+1}-01-01",
+    )
+    logger.debug(
+        "Found %s shots in %s in the specified geometry", len(gedi_shots), year
+    )
+    if len(gedi_shots) == 0:
+        raise RuntimeError("Found 0 shots specified geometry and year.")
+
+    # Load JRC data within tile
+    logger.info("Loading JRC data from 1990 to %s", year)
+    first_deforestation = load_jrc_data(*geometry.bounds, dataset="DeforestationYear")
+    first_degradation = load_jrc_data(*geometry.bounds, dataset="DegradationYear")
+    annual_change = load_jrc_data(
+        *geometry.bounds, dataset="AnnualChange", years=list(range(1990, year + 1))
+    )
+
+    # Compute recovery period from JRC
+    logger.info("Computing recovery period until year %s", year)
+    recovery_period = compute_recovery_period(
+        annual_change, first_deforestation, first_degradation
+    )
+
+    # Extract JRC locations that correspond to GEDI shots
+    filtered_shots = overlay_gedi_shots_and_recovery_raster(gedi_shots, recovery_period)
+    dataset = pd.DataFrame(
+        filtered_shots,
+        columns=["shot_number", "recovery_period", "overlap_quality"],
+    )
+    return dataset
--- src/visualisation/visualisation_params.py
+++ src/visualisation/visualisation_params.py
@@ -1,6 +1,12 @@
 """Styling parameters and legend for visualisations"""
+import colorcet as cc
+import matplotlib as mpl
+from bokeh.models import FixedTicker
 
 opt_size = {"width": 750, "height": 320, "show_grid": True}
+opt_size2 = {"width": 550, "height": 350, "show_grid": True}
+opt_size3 = {"width": 550, "height": 160, "show_grid": True}
+
 
 opt_densities = {
     "colorbar": True,
@@ -53,3 +59,70 @@ legend = {
     "acd_difference": dict(name="ACD difference", unit="kgC/m2"),
     "chm_difference": dict(name="Canopy height difference", unit="m"),
 }
+
+# Satellite data visualisation parameters
+opt_esri = dict(
+    title="ESRI Satellite Imagery (2021)",
+    xlabel="Longitude",
+    ylabel="Latitude",
+)
+
+# Polygon options
+opt_poly = dict(fill_alpha=0.3, hover_fill_alpha=0.0, tools=["hover"])
+
+
+# Annual change visualisation parameters
+rgb = lambda r, g, b: "#%02x%02x%02x" % (r, g, b)
+annual_change_legend = {
+    1: dict(name="Undisturbed TMF", color=rgb(0, 90, 0)),
+    2: dict(name="Degraded TMF", color=rgb(100, 135, 35)),
+    3: dict(name="Deforested land", color=rgb(255, 190, 45)),
+    4: dict(name="Forest regrowth", color=rgb(210, 250, 60)),
+    5: dict(
+        name="Other land cover", color=rgb(0, 140, 190)
+    ),  # Permanent or seasonal water
+    6: dict(name="Other land cover", color=rgb(255, 255, 255))
+    # 241: dict(name="fill value", color=rgb(0, 0, 0)),
+}
+
+opt_annual_change = dict(
+    tools=["hover"],
+    colorbar=True,
+    clabel="JRC Annual Change class",
+    cmap=mpl.colors.ListedColormap(
+        [val["color"] for val in annual_change_legend.values()]
+    ),
+    color_levels=[key for key in annual_change_legend],
+    colorbar_opts={
+        "ticker": FixedTicker(ticks=[key + 0.5 for key in annual_change_legend]),
+        "major_label_overrides": {
+            key + 0.5: val["name"] for key, val in annual_change_legend.items()
+        },
+    },
+    cformatter="%d",
+    xformatter="%.0f",
+    yformatter="%.0f",
+)
+
+# Change year visualisation parameters
+def opt_change_year(start_year=1985, end_year=2020):
+    return dict(
+        tools=["hover"],
+        colorbar=True,
+        clabel="Year",
+        cmap=mpl.colors.LinearSegmentedColormap.from_list(
+            "cmap", cc.bgy, N=end_year - start_year
+        ),
+        colorbar_opts={
+            "major_label_text_align": "left",
+            "ticker": FixedTicker(
+                ticks=[key - 0.5 for key in range(start_year, end_year + 1)]
+            ),
+            "major_label_overrides": {
+                key - 0.5: str(key) for key in range(start_year, end_year + 1)
+            },
+        },
+        cformatter="%d",
+        xformatter="%.0f",
+        yformatter="%.0f",
+    )
