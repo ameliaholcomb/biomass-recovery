e21cbf98fa5f93ace30da0580215baea01a7a32d
Simon Mathis <svm34@cam.ac.uk>
2021-07-13 09:18:51 +0100

    feature: change units of biomass formulas from kgC/m2 to MgC/ha

--- src/processing/canopy_height_models.py
+++ src/processing/canopy_height_models.py
@@ -10,16 +10,21 @@ def longo2016_acd(top_of_canopy_height: float) -> float:
         https://agupubs.onlinelibrary.wiley.com/action/downloadSupplement?
         doi=10.1002%2F2016GB005465&file=gbc20478-sup-0001-supplementary.pdf
 
+    Note:
+        The original formula is in units of kgC/m2. We therefore
+        multiply the result by 10 to get the ACD values in MgC/ha
+
     Args:
         top_of_canopy_height (float): Top of canopy height in meters
             (meant to work with mean TCH at 50x50m resolution as that
             is what was used in the creation of the formula)
 
     Returns:
-        float: Above ground carbon density in kgC/m2.
+        float: Above ground carbon density in MgC/ha.
     """
-    # Note: ACD units are in kgC/m2
-    return 0.054 * top_of_canopy_height ** 1.76
+    # Note: ACD units are in MgC/ha (hence multiplication by 10). To get units in
+    #  kgC/ha, remove multiplication by 10
+    return 10 * 0.054 * top_of_canopy_height ** 1.76
 
 
 def asner2014_acd(top_of_canopy_height: float) -> float:
@@ -30,21 +35,17 @@ def asner2014_acd(top_of_canopy_height: float) -> float:
     Source:
         https://www.sciencedirect.com/science/article/pii/S003442571300360X
 
-    Note:
-        The original formula is in units of MgC/ha. We therefore
-        divide the result by 10 to get the ACD values in kgC/m2
-
     Args:
         top_of_canopy_height (float): Top of canopy height in meters
             (meant to work with mean TCH at 50x50m resolution as that
             is what was used in the creation of the formula)
 
     Returns:
-        float: Above ground carbon density in kgC/m2.
+        float: Above ground carbon density in MgC/ha.
     """
     # Note: native ACD unit of formula is MgC/ha. To convert to
-    #  kgC/m2, we divide by 10.
-    return 6.8500 * top_of_canopy_height ** 0.9520 / 10.0
+    #  kgC/m2, divide by 10.
+    return 6.8500 * top_of_canopy_height ** 0.9520
 
 
 def asner2014_colombia_acd(top_of_canopy_height: float) -> float:
@@ -55,18 +56,14 @@ def asner2014_colombia_acd(top_of_canopy_height: float) -> float:
     Source:
         https://www.sciencedirect.com/science/article/pii/S003442571300360X
 
-    Note:
-        The original formula is in units of MgC/ha. We therefore
-        divide the result by 10 to get the ACD values in kgC/m2
-
     Args:
         top_of_canopy_height (float): Top of canopy height in meters
             (meant to work with mean TCH at 50x50m resolution as that
             is what was used in the creation of the formula)
 
     Returns:
-        float: Above ground carbon density in kgC/m2.
+        float: Above ground carbon density in MgC/ha.
     """
     # Note: native ACD unit of formula is MgC/ha. To convert to
-    #  kgC/m2, we divide by 10.
-    return 2.1011 * top_of_canopy_height ** 1.2682 / 10.0
+    #  kgC/m2, divide by 10.
+    return 2.1011 * top_of_canopy_height ** 1.2682
