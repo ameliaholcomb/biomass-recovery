0f6a1143a343fe3f5aced7e5f4f3cba19d027897
Amelia <amelia.holcomb@gmail.com>
2022-08-23 14:52:41 +0000

    [fix] update deprecated method for getting crs

--- src/data/gedi_database.py
+++ src/data/gedi_database.py
@@ -3,6 +3,7 @@ import logging
 import geopandas as gpd
 import numpy as np
 import pandas as pd
+import pyproj
 import sqlalchemy as db
 from sqlalchemy import create_engine, inspect
 
@@ -11,6 +12,7 @@ from src.utils.logging import get_logger
 
 logger = get_logger(__file__)
 logger.setLevel(logging.INFO)
+
 
 def gedi_sql_query(
     table_name: str,
@@ -29,13 +31,15 @@ def gedi_sql_query(
         conditions += [f"absolute_time between '{start_time}' and '{end_time}'"]
     # Spatial conditions
     if geometry is not None:
-        crs = gpd.tools.crs.CRS(crs)
+        crs = pyproj.CRS.from_user_input(crs)
         conditions += [
             "ST_Intersects(geometry, "
             f"ST_GeomFromText('{geometry.to_wkt().values[0]}', {crs.to_epsg()}))"
         ]
     # Combining conditions
-    condition = f" WHERE {' and '.join(conditions)}" if len(conditions) > 0 else ""
+    condition = (
+        f" WHERE {' and '.join(conditions)}" if len(conditions) > 0 else ""
+    )
     # Setting limits
     limits = f" LIMIT {limit}" if limit is not None else ""
 
@@ -44,20 +48,27 @@ def gedi_sql_query(
             "Warning! This will load the entire table. To proceed set `force`=True."
         )
 
-    sql_query = f"SELECT {', '.join(columns)} FROM {table_name}" + condition + limits
+    sql_query = (
+        f"SELECT {', '.join(columns)} FROM {table_name}" + condition + limits
+    )
     return sql_query
+
 
 class GediDatabase(object):
     """Database connector for the GEDI DB."""
+
     def __init__(self):
         self.engine = create_engine(DB_CONFIG, echo=False)
         self.inspector = inspect(self.engine)
         self.allowed_cols = {}
         for table_name in self.inspector.get_table_names():
-            allowed_cols = {col['name'] for col in self.inspector.get_columns(table_name)}
+            allowed_cols = {
+                col["name"] for col in self.inspector.get_columns(table_name)
+            }
             self.allowed_cols[table_name] = allowed_cols
 
-    def query(self,
+    def query(
+        self,
         table_name: str,
         columns: str = "*",
         geometry: gpd.GeoDataFrame = None,
@@ -70,7 +81,7 @@ class GediDatabase(object):
     ) -> pd.DataFrame:
 
         if table_name not in self.allowed_cols:
-            raise ValueError('Unsupported table {table_name}.')
+            raise ValueError("Unsupported table {table_name}.")
 
         if columns != "*":
             for column in columns:
@@ -96,7 +107,9 @@ class GediDatabase(object):
             )
 
             logger.debug("SQL Query: %s", sql_query)
-            return gpd.read_postgis(sql_query, con=self.engine, geom_col="geometry")
+            return gpd.read_postgis(
+                sql_query, con=self.engine, geom_col="geometry"
+            )
 
         else:
             # Construct sql query
